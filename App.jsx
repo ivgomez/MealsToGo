2ad2687b6aff40807d04c1ff8850366aa4640b3b
ivgomez <iv.gomez@hotmail.com>
2022-06-11 22:32:07 -0500

    class 112 - setting up theme

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/RestaurantsScreen";
+
+export default function App() {
+  return (
+    <>
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
