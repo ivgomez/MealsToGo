c75a71096d83f5ac07d2540b3796e9f3c67193db
ivgomez <iv.gomez@hotmail.com>
2022-06-14 07:29:28 -0500

    class 127

--- App.jsx
+++ App.jsx
@@ -1,11 +1,32 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
+import { theme } from "./src/infrastructure/theme";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { ThemeProvider } from "styled-components/native";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/RestaurantsScreen";
 
 export default function App() {
+  let [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  let [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
--- src/features/restaurants/screens/RestaurantsScreen.jsx
+++ src/features/restaurants/screens/RestaurantsScreen.jsx
@@ -21,11 +21,10 @@ const SafeArea = styled(SafeAreaView)`
 `;
 
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding: ${({ theme }) => theme.space[3]};
 `;
 
 const RestaurantListContainer = styled.View`
   flex: 1;
-  padding: 16px;
-  background-color: blue;
+  padding: ${({ theme }) => theme.space[3]};
 `;
